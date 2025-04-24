f758cdc6e55c5e9d712c70f4e1df8bfed1f7359b
Ishtiak13 <ishtiakalhumaidi@gmail.com>
2025-04-24 16:17:55 +0600

    mission accomplished (probably)

--- src/components/ui/CountUpCard.jsx
+++ src/components/ui/CountUpCard.jsx
@@ -6,7 +6,9 @@ const CountUpCard = ({image, count, section}) => {
         <div className="flex items-center justify-center w-10 h-10 mx-auto mb-3 rounded-full bg-indigo-50 sm:w-12 sm:h-12">
         <img src={image} alt="" />
         </div>
-        <h6 className="text-4xl font-bold text-deep-purple-accent-400">{<CountUp end={count} duration={10}></CountUp>}+</h6>
+        <h6 className="text-4xl font-bold text-deep-purple-accent-400">{<CountUp end={count} duration={10} delay={0.5}
+enableScrollSpy
+scrollSpyOnce ></CountUp>}+</h6>
         <p className="mb-2 font-bold text-md">{section}</p>
         
       </div>
